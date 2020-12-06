fd0cccf42b0b58f94273e6c41ed95bac161504e7
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-06 13:51:56 -0800

    Created PathController class with a few Route-type Member methods

--- routing/src/main/java/com/practice/routing/PathController.java
+++ routing/src/main/java/com/practice/routing/PathController.java
@@ -1,1 +1,22 @@
+package com.practice.routing;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PathController {
+	
+	@RequestMapping("/dojo")
+	public String path1() {
+		return "Oh yeah! I do want to learn Kung Fu!";
+	}
+	@RequestMapping("/{test1}-{test2}")
+	public String path2(@PathVariable("test1") String test1, @PathVariable("test2") String test2) {
+		return "Lol the URL says; " + test1 + " " + test2;
+	}
+	@RequestMapping("/{test3}/{test4}")
+	public String path3(@PathVariable("test1") String test1, @PathVariable("test2") String test2) {
+		return "Oh Hey! Look at this; " + Character.toUpperCase(test1.charAt(0)) + Character.toUpperCase(test2.charAt(0)) + " Lol I abbreviated the input!";
+	}
+}
