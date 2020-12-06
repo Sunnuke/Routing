74350096ae6251027102577d5d40b87d093cd578
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-06 15:59:42 -0800

    Still not clear but, Fixed internal server ERROR

--- routing/src/main/java/com/practice/routing/PathController.java
+++ routing/src/main/java/com/practice/routing/PathController.java
@@ -1,22 +1,26 @@
 package com.practice.routing;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class PathController {
 	
 	@RequestMapping("/dojo")
 	public String path1() {
 		return "Oh yeah! I do want to learn Kung Fu!";
 	}
-	@RequestMapping("/{test1}-{test2}")
+	@RequestMapping("/{test1}-{test2}/")
 	public String path2(@PathVariable("test1") String test1, @PathVariable("test2") String test2) {
 		return "Lol the URL says; " + test1 + " " + test2;
 	}
-	@RequestMapping("/{test3}/{test4}")
-	public String path3(@PathVariable("test1") String test1, @PathVariable("test2") String test2) {
-		return "Oh Hey! Look at this; " + Character.toUpperCase(test1.charAt(0)) + Character.toUpperCase(test2.charAt(0)) + " Lol I abbreviated the input!";
+	@RequestMapping("/a/{test3}-{test4}/")
+	public String path3(@PathVariable("test3") String test1, @PathVariable("test4") String test2) {
+		String a = String.valueOf(Character.toUpperCase(test1.charAt(0)));
+		String b = String.valueOf(Character.toUpperCase(test2.charAt(0)));
+		String say = "Oh Hey! Look at this; " + a + b + " Lol I abbreviated the input!";
+		return say;
 	}
 }
